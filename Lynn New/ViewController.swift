9b3566b1cb0c6953b35062866f0b1d29e8a0d648
tentiap <inooyabu@gmail.com>
2021-07-24 14:13:46 +0700

    Initial Commit

---
+++ Lynn New/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lynn New
+//
+//  Created by Tenti Atika Putri on 24/07/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
